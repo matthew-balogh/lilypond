dbaeea1a24f38743ee5bdc629672d3886796a880
Matthew Balogh <meld-03-spouts@icloud.com>
2025-10-08 17:22:06 +0200

    #1 Add legacy version

--- lilypond/pond.py
+++ lilypond/pond.py
@@ -4,6 +4,7 @@ import numpy as np
 from minisom import MiniSom
 from typing import Literal
 from matplotlib.colors import ListedColormap
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 class Pond:
     def __init__(self, som: MiniSom, data, som_distance_scaling: Literal["sum", "mean"]="mean", verb=False):
@@ -122,6 +123,12 @@ class Pond:
 
         if self.verb: print(f"Pond is visualized.")
 
+    def legacy(self):
+        if not hasattr(self, "legacyPond_") or self.legacyPond_ is None:
+            self.legacyPond_ = LegacyPond(self, self.verb)
+        
+        return self.legacyPond_
+
     def __place_petals(self, cx, cy, hit_num, petal_magnifier, petal_min_gap_fraction, pixel_width, linewidth, color, opacity, ax):
         if hit_num == 0:
             return
@@ -142,3 +149,35 @@ class Pond:
         
         center_dot = plt.Circle((cx, cy), length * 0.1, color="#FFC800", zorder=6, alpha=opacity)
         ax.add_patch(center_dot)
+
+
+class LegacyPond:
+    def __init__(self, pond: Pond, verb=False):
+        self.pond = pond
+        self.verb = verb
+
+        if self.verb: print("Legacy pond is created.")
+    
+    def visualize_hitmap(self, cmap="binary", ax=None, title="Hitmap"):
+        if ax is None:
+            ax = plt.gca()
+        
+        ax.set_title(title)
+
+        im = ax.imshow(self.pond.hitmap_, origin="lower", cmap=cmap)
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="6%", pad=0.2)
+        cax.tick_params(labelsize=11)
+        cbar = plt.colorbar(im, cax=cax)
+
+    def visualize_distance_map(self, cmap="Spectral_r", ax=None, title="Distance map"):
+        if ax is None:
+            ax = plt.gca()
+        
+        ax.set_title(title)
+
+        im = ax.imshow(self.pond.distmap_, origin="lower", cmap=cmap)
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="6%", pad=0.2)
+        cax.tick_params(labelsize=11)
+        cbar = plt.colorbar(im, cax=cax)
