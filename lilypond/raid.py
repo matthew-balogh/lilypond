ca35ffe32900b2f0535a3891e1b717f8f33e051a
Matthew Balogh <meld-03-spouts@icloud.com>
2025-10-11 16:27:24 +0200

    Fix visualization of abnormal points

--- lilypond/raid.py
+++ lilypond/raid.py
@@ -13,37 +13,43 @@ class RaidedPond():
 
         if self.verb: print("Raided pond is created.")
 
-    def visualize(self):
+    def visualize(self, return_fig=True):
         som = self.pond.som
         distance_map = som.distance_map()
+        abnormal_win_map = som.win_map(self.data_abnormal)
 
-        x, y = zip(*som.win_map(self.data_abnormal).keys())
-        z = np.linalg.norm(self.data_abnormal - som.quantization(self.data_abnormal), axis=1)
+        triplets = [
+            (i, j, val)
+            for (i, j), val_list in abnormal_win_map.items()
+            for val in val_list
+        ]
 
-        m, n = self.pond.lattice_shape_
-        x_coords = np.linspace(0, n-1, n)
-        y_coords = np.linspace(0, m-1, m)
-        xx, yy = np.meshgrid(x_coords, y_coords)
+        bmu_x, bmu_y, v = zip(*triplets)
+        bmu_dist = np.linalg.norm(self.data_abnormal - som.quantization(self.data_abnormal), axis=1)
+
+        r, c = self.pond.lattice_shape_
+        gridX, gridY = np.linspace(0, c-1, c), np.linspace(0, r-1, r)
+        gridXX, gridYY = np.meshgrid(gridX, gridY)
 
         fig = go.Figure()
 
         # raid points
         fig.add_trace(go.Scatter3d(
-            x=x, y=y, z=z,
+            x=bmu_x, y=bmu_y, z=bmu_dist,
             mode='markers',
-            marker=dict(size=5, color='black', opacity=1, symbol="cross")
+            marker=dict(size=3, color='black', opacity=1, symbol="cross")
         ))
 
         # raid point projections on XY-plane
         fig.add_trace(go.Scatter3d(
-            x=x, y=y, z=np.zeros_like(z),
+            x=bmu_x, y=bmu_y, z=np.zeros_like(bmu_dist),
             mode='markers',
-            marker=dict(size=5, color='grey', symbol='circle'),
+            marker=dict(size=1, color='grey', symbol='square'),
             name='Projections'
         ))
 
         # projection lines
-        for xi, yi, zi in zip(x, y, z):
+        for xi, yi, zi in zip(bmu_x, bmu_y, bmu_dist):
             fig.add_trace(go.Scatter3d(
                 x=[xi, xi], y=[yi, yi], z=[0, zi],
                 mode='lines',
@@ -54,14 +60,12 @@ class RaidedPond():
 
         # pond surface
         fig.add_trace(go.Surface(
-            x=xx, y=yy, z=np.zeros_like(xx),
+            x=gridXX, y=gridYY, z=np.zeros_like(gridXX),
             surfacecolor=np.flipud(distance_map),
             colorscale='Viridis_r',
             showscale=True,
             opacity=0.7
         ))
 
-        # fig.update_layout(scene=dict(zaxis=dict(range=[0, 1])))
-        fig.show()
-
-        return fig
+        if return_fig: return fig
+        else: fig.show()
